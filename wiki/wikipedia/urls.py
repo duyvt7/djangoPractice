c887b0d6aa07258c6191e1bdff7eba2de976c95d
duyvt <vuthanhduy93@gmail.com>
2021-08-08 22:51:09 +0700

    all pages create

--- wiki/wikipedia/urls.py
+++ wiki/wikipedia/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('css', views.css, name= 'css'),
+    path('html', views.html, name= 'html'),
+    path('git', views.git, name= 'git'),
+    path('python', views.python, name= 'python'),
+    path('Django', views.Django, name= 'Django'),
 ]
--- wiki/wikipedia/views.py
+++ wiki/wikipedia/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'wikipedia/index.html')
+def css(request):
+    return render(request, 'wikipedia/css.html')
+def html(request):
+    return render(request, 'wikipedia/html.html')
+def git(request):
+    return render(request, 'wikipedia/git.html')
+def python(request):
+    return render(request, 'wikipedia/python.html')
+def Django(request):
+    return render(request, 'wikipedia/Django.html')
